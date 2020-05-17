ded1ab8cc68b233dc568e4fe64977f5b8c7dedbf
Surendra Galwa <surendra.galwa@nymo.ai>
2020-05-17 23:39:40 +0530

    Application and CRUD setup (WIP)

---
+++ app/models/user.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    contactNumber = db.Column(db.Integer)
+    address = db.Column(db.String(25))
+    role = db.Column(db.String(20))
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+    def __init__(self, email, password, contactNumber, address, role):
+        self.email = email
+        self.password = password
+        self.contactNumber = contactNumber
+        self.address = address
+        self.role = role
+    def __repr__(self):
+        return '' % self.id
+# db.create_all()
+
+class UserSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = User
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    email = fields.String(required=True)
+    password = fields.String(required=True)
+    contactNumber = fields.Number(required=True)
+    address = fields.String(required=True)
+    role = fields.String(required=True)
+
+# def initModels(db):
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask_restful import Api
 
 # from app import app
-from app.modules.users import Users
+from app.modules.users import Users, User
 
 def initRoutes(app):
     api = Api(app)
     # add user routes
-    api.add_resource(Users, '/user')
+    api.add_resource(Users, '/users')
+    api.add_resource(User, '/user/<int:userId>')
